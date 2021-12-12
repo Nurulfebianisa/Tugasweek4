41d5a5b8a394fc2e5aaf59ce3bd59184b93af3ae
Nurulfebianisa <92684819+Nurulfebianisa@users.noreply.github.com>
2021-12-12 20:09:48 +0700

    Add files via upload

---
+++ src/main/kotlin/Perpangkatan.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var pangkat = "6 ^ 2"
+    println("pangkat: $pangkat")
+    var hasil1 = "36"
+    println(" 36")
+
+    var pangkat1 = "4 ^ 2"
+    println("pangkat: $pangkat")
+    var hasil = "36"
+    println(" 16")
+}
+
+
