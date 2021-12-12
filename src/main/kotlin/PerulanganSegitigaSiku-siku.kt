83896152afe194e8c8d42f4a6204609af53d0894
Nurulfebianisa <nurulfebyanisa26@gmail.com>
2021-12-12 07:50:06 +0700

    Initial commit

---
+++ src/main/kotlin/PerulanganSegitigaSiku-siku.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    repeat(1){
+        println("1")
+    }
+
+    repeat(1){
+        println("1 2")
+    }
+
+    repeat(1){
+        println("1 2 3")
+    }
+
+    repeat(1){
+        println("1 2 3 4")
+    }
+}
+
